ac304edbdc802cb3389ee11e0fa291dcecbdb4e9
tanghongxin <hongxin.tang@hotmail.com>
2021-06-07 23:19:22 +0800

    📝 初始化配置与 README

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,17 @@
+***REMOVED***
+
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    // displayAllHeaders: true,
+    sidebar: 'auto'
+  ***REMOVED***,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname)
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
 ***REMOVED***
