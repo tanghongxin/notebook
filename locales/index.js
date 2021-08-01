dc89d8aceda3f73c1fff54bd9e357ed775b2a0c9
tanghongxin <hongxin.tang@hotmail.com>
2021-08-01 09:32:06 +0800

    🌐 (locales) 增加「高德地图」汉化

--- locales/index.js
+++ locales/index.js
@@ -3,5 +3,6 @@ module.exports = new Map([
   ['data-structure', '数据结构'],
   ['database', '数据库'],
   ['detailed-design', '详细设计'],
-  ['wechat-mini-programs', '微信小程序']
+  ['wechat-mini-programs', '微信小程序'],
+  ['amap', '高德地图'],
 ])
